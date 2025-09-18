749fcdcb3bbf9feeeb3c2bec4018e0cfbae5c166
Ayan Shah <ashah34792@gmail.com>
2025-09-18 11:43:54 +0000

    datatype

---
+++ basics/datatype.js
@@ -0,0 +1,41 @@
+// alert("hey do not use any sound extention") 
+//this is work only on browser not on nodejs terminal
+
+//string 
+let name = "ayan"   
+
+//number
+let age = 18        
+
+// BigInt
+
+//bolean => has only trur or false
+let IsLoggedIn = true
+
+//undefine value not assign
+let state            
+
+//null is a standalone value 
+let mobileNumber_2 = null  
+// null is a object when we check typeof null
+console.log(typeof null);
+
+
+//    Symbol is used for find unique ness
+
+//object ! 
+
+let personOne = {firstName:"John", lastName:"Doe"};
+console.log(personOne.firstName,personOne.lastName);
+
+// document.getElementById("demo").innerHTML = personOne.firstName + " " + personOne.lastName;
+// for display on browser document.....
+// document.getElementById is not display on node js terminal
+
+console.log("the age is " + age );
+
+
+console.log("the type of age is " + typeof age + " datatype");
+
+
+
